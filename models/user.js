699d77f8f746deedecdb98f13588d000fe62507c
syifacts <ss4166269@gmail.com>
2024-12-14 13:06:58 +0700

    first commit

--- models/user.js
+++ models/user.js
@@ -3,10 +3,13 @@ const mongoose = require('mongoose');
 // Membuat model User
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true }, // Pastikan username unik
   password: { type: String, required: true },
+  userId: { type: String, unique: true, default: () => new mongoose.Types.ObjectId() }, // Optional, jika ingin menggunakan custom userId
+  token: { type: String, default: null }, // Token untuk autentikasi, misalnya JWT
 });
 
+// Membuat model berdasarkan schema
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
